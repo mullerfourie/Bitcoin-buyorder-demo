c70b0342041ea207c91a05c342a5e7d3323bc55f
Müller Fourie <mullerfourie@gmail.com>
2018-05-09 11:49:34 +0200

    MongoClient connection pooling refactor

--- build/server/server.js
+++ build/server/server.js
@@ -18,26 +18,30 @@ var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
 var _mongodb = require('mongodb');
 
-var _mongodb2 = _interopRequireDefault(_mongodb);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var MONGO_URL = 'mongodb://localhost:27017';
 var port = process.env.PORT || 3000;
 var app = (0, _express2.default)();
-
 var publicPath = _express2.default.static(_path2.default.join(__dirname, '../')); // refers to my /build root folder
 var indexPath = _path2.default.join(__dirname, '../index.html');
+var MONGO_URL = 'mongodb://localhost:27017';
+var database = void 0;
 
 app.use(publicPath);
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use(_bodyParser2.default.json());
 
-app.listen(port, function (error) {
-  if (error) {
-    console.log(error); // Log errors
-  }
-  console.log('listening at http://localhost:' + port);
+// set up connection pool
+_mongodb.MongoClient.connect(MONGO_URL, function (error, db) {
+  if (error) throw error;
+
+  database = db.db('testDatabase');
+  app.listen(port, function (error) {
+    if (error) {
+      console.log(error); // Log errors
+    }
+    console.log('listening at http://localhost:' + port);
+  });
 });
 
 app.get('/', function (req, res) {
@@ -46,31 +50,20 @@ app.get('/', function (req, res) {
 
 app.post('/buy-order', function (req, res) {
   // adds a new buy order TODO: needs security
-  _mongodb2.default.MongoClient.connect(MONGO_URL, function (error, db) {
-    if (error) throw error;
-
-    var database = db.db('testDatabase');
-    database.collection('buyorders').insertOne(req.body, function (err) {
-      if (err) {
-        res.sendStatus(500); // internal server error
-      }
-      res.redirect('/');
-    });
+  database.collection('buyorders').insertOne(req.body, function (err) {
+    if (err) {
+      res.sendStatus(500); // internal server error
+    }
+    res.redirect('/');
   });
 });
 
 app.get('/get-orders', function (req, res) {
-  _mongodb2.default.MongoClient.connect(MONGO_URL, function (error, db) {
-    if (error) throw error;
-
-    var database = db.db('testDatabase');
-    // dump all the documents -> not good in the long run
-    database.collection('buyorders').find({}).toArray(function (err, docs) {
-      if (err) {
-        res.sendStatus(500); // internal server error
-      }
-      res.json(docs);
-    });
+  database.collection('buyorders').find({}).toArray(function (err, docs) {
+    if (err) {
+      res.sendStatus(500); // internal server error
+    }
+    res.json(docs);
   });
 });
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,24 +1,30 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
-import mongo from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-const MONGO_URL = 'mongodb://localhost:27017';
 const port = process.env.PORT || 3000;
 const app = express();
-
 const publicPath = express.static(path.join(__dirname, '../')); // refers to my /build root folder
 const indexPath = path.join(__dirname, '../index.html');
+const MONGO_URL = 'mongodb://localhost:27017';
+let database;
 
 app.use(publicPath);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(port, (error) => {
-  if (error) {
-    console.log(error); // Log errors
-  }
-  console.log(`listening at http://localhost:${port}`);
+// set up connection pool
+MongoClient.connect(MONGO_URL, (error, db) => {
+  if (error) throw error;
+
+  database = db.db('testDatabase');
+  app.listen(port, (error) => {
+    if (error) {
+      console.log(error); // Log errors
+    }
+    console.log(`listening at http://localhost:${port}`);
+  });
 });
 
 app.get('/', (req, res) => {
@@ -26,32 +32,20 @@ app.get('/', (req, res) => {
 });
 
 app.post('/buy-order', (req, res) => { // adds a new buy order TODO: needs security
-  mongo.MongoClient.connect(MONGO_URL, (error, db) => {
-    if (error) throw error;
-
-    const database = db.db('testDatabase');
-    database.collection('buyorders').insertOne(req.body, (err) => {
-      if (err) {
-        res.sendStatus(500); // internal server error
-      }
-      res.redirect('/');
-    });
+  database.collection('buyorders').insertOne(req.body, (err) => {
+    if (err) {
+      res.sendStatus(500); // internal server error
+    }
+    res.redirect('/');
   });
 });
 
 app.get('/get-orders', (req, res) => {
-  mongo.MongoClient.connect(MONGO_URL, (error, db) => {
-    if (error) throw error;
-
-    const database = db.db('testDatabase');
-    // dump all the documents -> not good in the long run
-    database.collection('buyorders').find({}).toArray(function(err, docs) {
-      if (err) {
-        res.sendStatus(500); // internal server error
-      }
-      res.json(docs);
-    });
-    
+  database.collection('buyorders').find({}).toArray(function(err, docs) {
+    if (err) {
+      res.sendStatus(500); // internal server error
+    }
+    res.json(docs);
   });
 });
 
